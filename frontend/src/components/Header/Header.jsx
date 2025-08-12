3471b17d00f7826b58208596cdc54d0b2973dae7
Aakash <112680901+aakashgargdev@users.noreply.github.com>
2025-08-12 07:26:46 +0530

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className='header'>
         <div className="header-contents">
             <h2>Order your favourite food here</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dolorem, dicta itaque iure fugiat architecto.</p>
+            <p>Enjoy a diverse menu of delicious dishes crafted with the finest ingredients. At FeedFood, we’re here to satisfy your cravings and elevate your dining experience—one flavorful bite at a time.</p>
             <button >View Menu</button>
         </div>
     </div>
